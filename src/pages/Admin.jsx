807c31152efbb111b5501402cfbbb030a2f3f70e
ruchbiahadian <87080676+ruchbiahadian@users.noreply.github.com>
2023-03-08 01:09:11 +0700

    rev-3

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,5 +1,6 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
-import {Link} from "react-router-dom";
+import {useNavigate, Link} from "react-router-dom";
 
 const Admin = () =>{
 
