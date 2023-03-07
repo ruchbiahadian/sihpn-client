9a2e092599996ece2235e527a7055d31a4296116
ruchbiahadian <87080676+ruchbiahadian@users.noreply.github.com>
2023-03-07 22:38:19 +0700

    rev-2

--- src/pages/Add.Nasabah.jsx
+++ src/pages/Add.Nasabah.jsx
@@ -36,9 +36,10 @@ const AddNasabah = () =>{
     const handleClick = async (e) =>{
         e.preventDefault() // prevent page refresh
         try {
-            const resultId = await axios.post("http://localhost:8800/admin/nasabah/tambah", sentra, {withCredentials: true})
-            await axios.put("http://localhost:8800/admin/nasabah/update/kodeunik/" + (resultId.data.insertId + "x" + sentra.priode), {withCredentials: true});
-            await axios.post("http://localhost:8800/admin/hadiah/tambah/" + (resultId.data.insertId + "x" + sentra.id_sentra), {withCredentials: true});
+            await axios.put("http://localhost:8800/admin/nasabah/update/kodeunik/", {withCredentials: true});
+            // const resultId = await axios.post("http://localhost:8800/admin/nasabah/tambah", sentra, {withCredentials: true});
+            // await axios.put("http://localhost:8800/admin/nasabah/update/kodeunik/" + (resultId.data.insertId + "x" + sentra.priode), {withCredentials: true});
+            // await axios.post("http://localhost:8800/admin/hadiah/tambah/" + (resultId.data.insertId + "x" + sentra.id_sentra), {withCredentials: true});
             navigate("/admin/nasabah")
         } catch (err) {
             console.log(err)
