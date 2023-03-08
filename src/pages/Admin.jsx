6d8556e20f86688095b1059078e065754b823e4d
ruchbiahadian <87080676+ruchbiahadian@users.noreply.github.com>
2023-03-08 10:05:29 +0700

    rev-3

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -8,7 +8,7 @@ const Admin = () =>{
 
     const handleClick = () =>{
         try {
-                const res = axios.get("http://localhost:8800/logout", {withCredentials: true})
+                axios.get("http://localhost:8800/logout", {withCredentials: true})
                 alert("Anda berhasil logout!")
                 navigate("/")
         } catch (err) {
