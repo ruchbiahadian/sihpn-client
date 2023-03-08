a4c32cfdfdc4eaa5fd84eee9795dd9bf9750ff54
ruchbiahadian <87080676+ruchbiahadian@users.noreply.github.com>
2023-03-08 23:11:26 +0700

    rev-4

--- src/pages/Add.Nasabah.jsx
+++ src/pages/Add.Nasabah.jsx
@@ -68,6 +68,8 @@ const AddNasabah = () =>{
                     <option value="0">Sesi Pendampingan</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
                 </select>
                 <input type="number" placeholder="total poin" onChange={handleChange} name="poin" />
                 <button className="formButton" onClick={handleClick}>Tambah</button>
