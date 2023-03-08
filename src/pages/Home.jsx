48bc02d33c34550a0444d04d76c44968d706a6af
ruchbiahadian <87080676+ruchbiahadian@users.noreply.github.com>
2023-03-08 18:12:56 +0700

    rev-4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,6 +22,7 @@ const Home = () =>{
         e.preventDefault() // prevent page refresh
         try {
             const res = await axios.get("https://sihpn-server-production.up.railway.app/" + home.kode_unik, {withCredentials: true})
+            alert("length", res.data.length)
             const hasil = res.data[0];
             hasil.pengajuan = statusConverter(hasil.pengajuan);
             hasil.pembelian = statusConverter(hasil.pembelian);
